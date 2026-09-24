574c6abf7b35bbf925c5858d1db0123650292cf2
Behruz-05 <behruzxolmurodov00@gmail.com>
2026-09-24 17:31:03 +0500

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,25 @@
+# ismlar = ['Ali', 'Behruz', 'Muhammad', 'Sardorbek', 'Shahboz']
+#
+# print(ismlar)
+
+import random
+
+ismlar = ['Akbar', 'Jasur', 'Navruz', 'Azimbek', 'Sherzod']
+
+tanlangan_ism = random.choice(ismlar)
+tanlangan_ism2 = random.choice(ismlar)
+print("Salom", tanlangan_ism, "bugun choyxona bormi?")
+print(tanlangan_ism2, "choyxonaga boramizmi?")
+
+sonlar = [5, 45, 3, 43, 6]
+
+randomnumber = random.choice(sonlar)
+randomnumber2 = random.choice(sonlar)
+
+print(randomnumber+randomnumber2)
+
+print(randomnumber-randomnumber2)
+
+print(randomnumber*randomnumber2)
+
+print(randomnumber/randomnumber2)
